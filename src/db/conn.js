1b1f9e172877467d102449dedc02f8150e8b2176
resel143 <reshulwate@gmail.com>
2021-09-14 17:21:25 +0530

    wh

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,11 @@
-mongoURI = 'mongodb+srv://reshul:reshul%40123@newsfeeddeploy01.mv98l.mongodb.net/NewsFeed';
+// mongoURI = 'mongodb+srv://reshul:reshul%40123@newsfeeddeploy01.mv98l.mongodb.net/NewsFeed';
 
 // MONGOLAB_URI = "mongodb+srv://reshul:reshul%40123@newsfeeddeploy01.mv98l.mongodb.net/NewsFeed"
 
 
 const mongoose = require("mongoose");
 
-mongoose.connect(MONGODB_URI || mongoURI,{
+mongoose.connect("mongodb+srv://reshul:reshul%40123@newsfeeddeploy01.mv98l.mongodb.net/NewsFeed",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
