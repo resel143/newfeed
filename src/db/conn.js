8532e6d2c4b8b8fb7fdaccaeb4fbc51c2d3e1f48
resel143 <reshulwate@gmail.com>
2021-09-14 11:16:57 +0530

    make it better

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/NewsFeed",{
+mongoose.connect("mongodb+srv://reshul:reshul@123@newsfeeddeploy01.mv98l.mongodb.net/news?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
